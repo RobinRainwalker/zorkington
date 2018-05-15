5ac1e0bb744da081d428f528ebc510ac7c08b08d
Robin Rainwalker <robinrainwalker@robins-mbp.home>
2018-05-15 18:06:06 -0400

    cleaned up the console.logs

--- zork_test.js
+++ zork_test.js
@@ -7,34 +7,27 @@ let cozy_home_rooms = {
 let current_room = "mudroom"
 let user_input
 
-console.log("you are currently standing in " + current_room)
-console.log("you can move into the " + cozy_home_rooms[current_room].can_move_to)
+function introduction() {
+  console.log("welcome to mini-zork, you can move from one room to the next by entering the room you want to move to")
+  console.log("you are currently standing in " + current_room)
+  console.log("you can move into the " + cozy_home_rooms[current_room].can_move_to)
+}
+introduction()
 
 function select_room() {
   process.stdin.on('data', (a) => {
     user_input = a.toString().trim();
-    // console.log("user input " + user_input + " from select room function")
     // check_valid_transition(user_input)
     change_room(user_input)
-    console.log("you are currently standing in " + current_room)
-    console.log("you can move into the " + cozy_home_rooms[current_room].can_move_to)
+    where_am_i()
 
   })
-
-  // console.log("user input from AFTER check valid tansition " + user_input)
 }
 
-function check_valid_transition(attempted_room_transition) {
-  console.log("user input from DURING check valid transition " + user_input)
-  if (attempted_room_transition === "living room") {
-    console.log("attempting to move from " + current_room + " to " + cozy_home_rooms[current_room].can_move_to);
-  } else if (attempted_room_transition === "mudroom") {
-    console.log("attempting to move from " + current_room + " to " + cozy_home_rooms[current_room].can_move_to);
-  } else {
-    console.log("nah bae")
-  }
+function where_am_i() {
+  console.log("you are currently standing in " + current_room)
+  console.log("you can move into the " + cozy_home_rooms[current_room].can_move_to)
 }
-
 
 
 function change_room(new_room) {
@@ -46,8 +39,21 @@ function change_room(new_room) {
   }
 }
 
-
 select_room()
+
+// function check_valid_transition(attempted_room_transition) {
+//   console.log("user input from DURING check valid transition " + user_input)
+//   if (attempted_room_transition === "living room") {
+//     console.log("attempting to move from " + current_room + " to " + cozy_home_rooms[current_room].can_move_to);
+//   } else if (attempted_room_transition === "mudroom") {
+//     console.log("attempting to move from " + current_room + " to " + cozy_home_rooms[current_room].can_move_to);
+//   } else {
+//     console.log("nah bae")
+//   }
+// }
+
+
+
 
 
 // function check_variables() {
