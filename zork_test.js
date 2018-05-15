f809c0421bab07b2659d578af8321138cc610974
Robin Rainwalker <robinrainwalker@robins-mbp.home>
2018-05-15 17:45:27 -0400

    basic zork working. can move from mudroom to living room to kitchen and back

--- zork_test.js
+++ zork_test.js
@@ -1,6 +1,7 @@
 let cozy_home_rooms = {
   "mudroom": {can_move_to: ["living room"]},
-  "living room": {can_move_to: ["mudroom"]}
+  "living room": {can_move_to: ["mudroom", "kitchen"]},
+  "kitchen": {can_move_to: ["living room"]}
 }
 
 let current_room = "mudroom"
@@ -10,22 +11,18 @@ console.log("you are currently standing in " + current_room)
 console.log("you can move into the " + cozy_home_rooms[current_room].can_move_to)
 
 function select_room() {
-  process.stdin.once('data', (a) => {
+  process.stdin.on('data', (a) => {
     user_input = a.toString().trim();
-    console.log("user input " + user_input + " from select room function")
-    check_valid_transition(user_input)
+    // console.log("user input " + user_input + " from select room function")
+    // check_valid_transition(user_input)
+    change_room(user_input)
+    console.log(current_room + " current room")
   })
 
-  console.log("user input from AFTER check valid tansition " + user_input)
+  // console.log("user input from AFTER check valid tansition " + user_input)
 }
 
-select_room()
-
-
-function change_room(new_room) {
-}
-
-function check_valid_transition(attempted_room_transition) {
+// function check_valid_transition(attempted_room_transition) {
   console.log("user input from DURING check valid transition " + user_input)
   if (attempted_room_transition === "living room") {
     console.log("attempting to move from " + current_room + " to " + cozy_home_rooms[current_room].can_move_to);
@@ -35,6 +32,20 @@ function check_valid_transition(attempted_room_transition) {
     console.log("nah bae")
   }
 }
+
+
+
+function change_room(new_room) {
+  valid_moves = cozy_home_rooms[current_room].can_move_to
+  if (valid_moves.includes(new_room)) {
+    current_room = new_room
+  } else {
+    "uh oh, moving from " + current_room + " to " + new_room + " didn't work"
+  }
+}
+
+
+select_room()
 
 
 // function check_variables() {
