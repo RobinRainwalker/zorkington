3332bec6acb1b5d53ee01cb455b35459bc044762
RobinRainwalker <robin.rainwalker@gmail.com>
2018-06-06 10:04:07 -0400

    working on adding depth to each room via objects

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # zorkington
-this is a very very very basic version of zork that just lets the user move from one room to the next. it's a node.js program that is in it's infancy, please be gentle
+This is a very very very basic version of zork that just lets the user move from one room to the next. it's a node.js program that is in it's infancy, please be gentle
+
+
--- zork_test.js
+++ zork_test.js
@@ -1,8 +1,23 @@
 let cozy_home_rooms = {
-  "mudroom": {can_move_to: ["living room"]},
-  "living room": {can_move_to: ["mudroom", "kitchen"]},
-  "kitchen": {can_move_to: ["living room"]}
+  "mudroom": {
+		can_move_to: ["living room"]
+		},
+  "living room": {
+		can_move_to: ["mudroom", "kitchen"], 
+		contains: ["old ratty af sofa ", "1990's era TV"],
+		objects: {
+			name: ["sofa", "couch"],
+			description: "old ratty sofa",
+			actions: "sit",
+			color: "blue"
+			}
+		
+		},
+  "kitchen": {
+		can_move_to: ["living room"]
+		}
 }
+
 
 let current_room = "mudroom"
 let user_input
@@ -20,15 +35,26 @@ function select_room() {
     // check_valid_transition(user_input)
     change_room(user_input)
     where_am_i()
-
+	room_contains()
   })
+}
+
+//function interact
+
+function room_contains() {
+	if (cozy_home_rooms[current_room].contains == undefined) {
+		return
+	} else {
+	// cozy_home_rooms[current_room].objects.description.forEach(function(element){console.log(element);})
+}
+	console.log("as you enter this room, you notice " + cozy_home_rooms[current_room].contains)
+	
 }
 
 function where_am_i() {
   console.log("you are currently standing in " + current_room)
   console.log("you can move into the " + cozy_home_rooms[current_room].can_move_to)
 }
-
 
 function change_room(new_room) {
   valid_moves = cozy_home_rooms[current_room].can_move_to
@@ -40,24 +66,3 @@ function change_room(new_room) {
 }
 
 select_room()
-
-// function check_valid_transition(attempted_room_transition) {
-//   console.log("user input from DURING check valid transition " + user_input)
-//   if (attempted_room_transition === "living room") {
-//     console.log("attempting to move from " + current_room + " to " + cozy_home_rooms[current_room].can_move_to);
-//   } else if (attempted_room_transition === "mudroom") {
-//     console.log("attempting to move from " + current_room + " to " + cozy_home_rooms[current_room].can_move_to);
-//   } else {
-//     console.log("nah bae")
-//   }
-// }
-
-
-
-
-
-// function check_variables() {
-// console.log(user_input + " from check_variables function")
-// }
-
-// setTimeout( check_variables, 1000)
