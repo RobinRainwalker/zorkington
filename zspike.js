a86fd3910af5dc84c86f1b982023fb31b8661296
RobinRainwalker <robin.rainwalker@gmail.com>
2018-06-11 14:30:41 -0400

    got functions inside objects working, meaning stuff can now be interacted with

--- zspike.js
+++ zspike.js
@@ -1,39 +1,13 @@
 let state_object = {
 	"state_1": {
-		can_transition_state_to: ["state_2"],
-		actions_on_current_state: {
-			action_1: "action_1"
-		},
-		acted_actions_on_current_state: [],
-		interactable_within_current_state: {
-			interaction_1: function() {
-				let acted = state_object["state_1"].acted_actions_on_current_state 
-				acted.push(state_object["state_1"].acted_actions_on_current_state.action_1)
-				}
-		}
+		actionable: "action_1",
+		acted_on: [],
+		interactable: function() {
+			let acted = state_object["state_1"].acted_on
+			acted.push(state_object["state_1"].actionable)
+			}
 	}
 }
-
-
-//~ function test_action() {
-	//~ state_object["state_1"].interactable_within_current_state.interaction_1()	
-//~ }
-//~ test_action()
-
-function test() {
-console.log(state_object["state_1"].acted_actions_on_current_state + " acted")
-}
-state_object["state_1"].interactable_within_current_state.interaction_1()
-//~ test()
-
-
-//~ let state_object = {
-	//~ "state_1": {
-		//~ actionable: "action_1",
-		//~ acted_on: [],
-		//~ interactable: 
-	//~ }
-//~ }
 
 //~ function test() {
 	//~ let acted = state_object["state_1"].acted_on
@@ -41,4 +15,5 @@ state_object["state_1"].interactable_within_current_state.interaction_1()
 //~ }
 //~ test()
 
-//~ console.log(state_object["state_1"].acted_on[0])
+state_object["state_1"].interactable();
+console.log(state_object["state_1"].acted_on[0] + " I think I did it?")
