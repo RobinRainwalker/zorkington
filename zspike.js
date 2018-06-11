8abbf0777b860651f62e3ce6f91fe9c65e0d97d5
RobinRainwalker <robin.rainwalker@gmail.com>
2018-06-11 16:20:30 -0400

    class experimentation & object generation successfully

--- zspike.js
+++ zspike.js
@@ -1,13 +1,15 @@
-let state_object = {
-	"state_1": {
-		actionable: "action_1",
-		acted_on: [],
-		interactable: function() {
-			let acted = state_object["state_1"].acted_on
-			acted.push(state_object["state_1"].actionable)
-			}
-	}
-}
+//FUNCTIONS IN OBJECTS WITH STATE EXPERIMENTATION
+
+//~ let state_object = {
+	//~ "state_1": {
+		//~ actionable: "action_1",
+		//~ acted_on: [],
+		//~ interactable: function() {
+			//~ let acted = state_object["state_1"].acted_on
+			//~ acted.push(state_object["state_1"].actionable)
+			//~ }
+	//~ }
+//~ }
 
 //~ function test() {
 	//~ let acted = state_object["state_1"].acted_on
@@ -15,5 +17,36 @@ let state_object = {
 //~ }
 //~ test()
 
-state_object["state_1"].interactable();
-console.log(state_object["state_1"].acted_on[0] + " I think I did it?")
+//~ state_object["state_1"].interactable();
+//~ console.log(state_object["state_1"].acted_on[0] + " I think I did it?")
+
+
+// CLASS EXPERIMENTATION & OBJECT GENERATION
+class Room {
+	constructor(actionable, acted_on, interactable) {
+		this.actionable = actionable;
+		this.acted_on = acted_on;
+		this.interactable = interactable;
+	}
+}
+
+Room.prototype.action = function() {
+	console.log("you have " + this.acted_on)
+}
+
+
+//~ const newRoom = new Room("chaair", "sitted on tha thing", "ayup, u cans sit")
+
+//~ console.log(newRoom)
+
+//~ newRoom.action()
+
+var o = new Object();
+o.test = "testes"
+
+let newestRoom = new Room(o, 282, "hurpadurp")
+console.log(newestRoom)
+newestRoom.actionable.test = "other"
+console.log(newestRoom)
+
+newestRoom.action()
